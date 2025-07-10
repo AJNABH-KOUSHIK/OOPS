174a78c72f25e7b299772a54d92fd46077bb0f02
AJNABH-KOUSHIK <ajnabhb.ug24.cs@nitp.ac.in>
2025-07-10 19:33:48 +0530

    OBJECT EXAMPLE.java

---
+++ OBJECT /OBJECT EXAMPLE.java
@@ -0,0 +1,14 @@
+class Student {
+    // Property
+    String name = "Koushik";
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Creating an object of Student class
+        Student s1 = new Student();
+
+        // Printing the name using the object
+        System.out.println(s1.name);
+    }
+}
