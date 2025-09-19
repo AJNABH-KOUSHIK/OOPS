5f0903303a115892babe098edf40f4ad894fd5f0
AJNABH-KOUSHIK <ajnabhb.ug24.cs@nitp.ac.in>
2025-09-19 13:26:18 +0530

    Update 04. 3 TYPES OF CONSTRUCTORS USING OVERLOADING.java

--- 09. CONSTRUCTOR/TYPES OF CONSTRUCTORS/04. 3 TYPES OF CONSTRUCTORS USING OVERLOADING.java
+++ 09. CONSTRUCTOR/TYPES OF CONSTRUCTORS/04. 3 TYPES OF CONSTRUCTORS USING OVERLOADING.java
@@ -1,39 +1,49 @@
 package Lab6;
+
+class Employee {
+    int id;
+    String name;
+    int salary;
+
+    // Default constructor
+    Employee() {
+        this.id = 0;
+        this.name = "Not Assigned";
+        this.salary = 0;
+    }
+
+    // Constructor with id & name
+    Employee(int id, String name) {
+        this.id = id;
+        this.name = name;
+        this.salary = 30000;
+    }
+
+    // Constructor with id, name & salary
+    Employee(int id, String name, int salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = (salary < 0) ? 0 : salary; // prevent negative
+    }
+
+    // Display method
+    void display() {
+        System.out.println("The id of the Employee: " + this.id);
+        System.out.println("The name of the Employee: " + this.name);
+        System.out.println("The salary of the Employee: " + this.salary);
+        System.out.println("---------------------------");
+    }
+}
+
+// Main class
 public class TaskB {
     public static void main(String[] args) {
-        Employee e1=new Employee();
-        Employee e2=new Employee(1,"vijju");
-        Employee e3=new Employee(2,"kannu",10000);
+        Employee e1 = new Employee();                      // default
+        Employee e2 = new Employee(1, "Vijju");            // id+name
+        Employee e3 = new Employee(2, "Kannu", 10000);     // id+name+salary
+
         e1.display();
         e2.display();
         e3.display();
     }
-}
-class Employee{
-    int id;
-    String name;
-    int salary;
-Employee(){
-    this.id=0;
-    this.name="Not Assigned";
-    this.salary=0;
-}
-Employee(int id,String name){
-    this.id=id;
-    this.name=name;
-    this.salary=30000;
-}
-Employee(int id,String name,int salary){
-    this.id=id;
-    this.name=name;
-    this.salary=salary;
-    if(this.salary<0){
-        this.salary=0;
-    }
-}
-void display(){
-    System.out.println("The id of the Employee:"+this.id);
-    System.out.println("The name of the Employee:"+this.name);
-    System.out.println("The salary of the Employee:"+this.salary);
-}
 }
